880948873cdf9324c3b639167e0e55edd1ab0a6d
sonjamehes <97450837+sonjamehes@users.noreply.github.com>
2023-07-18 09:28:22 +0100

    ingestion complete for drivers file

--- notebooks/f1-project/ingestion/4. ingest_drivers_file_json_nested.py
+++ notebooks/f1-project/ingestion/4. ingest_drivers_file_json_nested.py
@@ -9,7 +9,49 @@
 
 # COMMAND ----------
 
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType
 
+# COMMAND ----------
+
+# display(dbutils.fs.mounts())
+
+# COMMAND ----------
+
+# %fs
+# ls /mnt/f1datalakelearn/raw-bronze
+
+# COMMAND ----------
+
+# first we create the schema for the nested object
+name_schema = StructType(fields= [
+                                StructField('forename', StringType(), True),
+                                StructField('surname', StringType(), True)
+])
+
+# COMMAND ----------
+
+driver_schema = StructType(fields= [
+                                StructField('driverId', IntegerType(), False),
+                                StructField('driverRef', StringType(), True),
+                                StructField('number', IntegerType(), True),
+                                StructField('code', StringType(), True),
+                                StructField('name', name_schema),
+                                StructField('dob', DateType(), True),
+                                StructField('nationality', StringType(), True),
+                                StructField('url', StringType(), True)
+
+])
+
+# COMMAND ----------
+
+drivers_df = spark.read \
+.schema(driver_schema) \
+.json('/mnt/f1datalakelearn/raw-bronze/drivers.json')
+
+
+# COMMAND ----------
+
+# drivers_df.printSchema()
 
 # COMMAND ----------
 
@@ -22,8 +64,49 @@
 
 # COMMAND ----------
 
+from pyspark.sql.functions import col, concat, current_timestamp, lit
+
+
+# COMMAND ----------
+
+
+drivers_df_renamed = drivers_df.withColumnRenamed('driverId', 'driver_id') \
+                               .withColumnRenamed ('driverRef', 'driver_red') \
+                               .withColumn('ingestion_date', current_timestamp()) \
+                               .withColumn('name', concat(col('name.forename'), lit(' '), col('name.surname')))
+
+# COMMAND ----------
+
+# display(drivers_df_renamed)
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ###Step 3. Drop unwanted columns
 # MAGIC 1. name.forname
 # MAGIC 2. name.surname
 # MAGIC 3. url
+
+# COMMAND ----------
+
+## forename and surname have been dropped when we renamed the column name
+
+final_df = drivers_df_renamed.drop('url')
+
+# COMMAND ----------
+
+# display(final_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###Step 4. Write output to processed-silver container in parquet format
+
+# COMMAND ----------
+
+final_df.write.mode('overwrite').parquet('/mnt/f1datalakelearn/processed-silver/drivers')
+
+# COMMAND ----------
+
+# %fs
+# ls /mnt/f1datalakelearn/processed-silver/drivers
