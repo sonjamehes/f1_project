59a378ba5640845f6970e8a014040a2dd6bf97e3
sonjamehes <97450837+sonjamehes@users.noreply.github.com>
2023-07-19 14:11:30 +0200

    commenting out everything and keeping the udemy version. mine has a dependency on driver's standings and I don't know if it will have any impact later in the project

--- notebooks/f1-project/transformations/3.constructor_standings_mine.py
+++ notebooks/f1-project/transformations/3.constructor_standings_mine.py
@@ -1,28 +1,32 @@
 # Databricks notebook source
-# MAGIC %run "../includes/config"
+# %run "../includes/config"
 
 # COMMAND ----------
 
-drivers_df = spark.read.parquet(f'{presentation_folder_path}/driver_standings')
+# drivers_df = spark.read.parquet(f'{presentation_folder_path}/driver_standings')
 
 # COMMAND ----------
 
-display(drivers_df)
+# display(drivers_df)
 
 # COMMAND ----------
 
-from pyspark.sql.functions import sum, when, col, count
+# from pyspark.sql.functions import sum, when, col, count
 
 # COMMAND ----------
 
-constructor_df = drivers_df \
-    .groupBy('race_year','team') \
-    .agg(sum('total_points').alias('total_points'), sum('wins').alias('wins'))
+# constructor_df = drivers_df \
+#     .groupBy('race_year','team') \
+#     .agg(sum('total_points').alias('total_points'), sum('wins').alias('wins'))
 
 # COMMAND ----------
 
-from pyspark.sql.functions import desc
+# from pyspark.sql.functions import desc
 
 # COMMAND ----------
 
-display(constructor_df.filter('race_year = 2020').orderBy(desc('wins')))
+# display(constructor_df.filter('race_year = 2020').orderBy(desc('wins')))
+
+# COMMAND ----------
+
+# constructor_df.mode('overwrite').parquet(f'{presentation_folder_path}/constructor_standings')
