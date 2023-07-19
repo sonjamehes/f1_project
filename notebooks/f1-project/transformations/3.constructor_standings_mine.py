ab0acbe61c698d72944fe7deaae81fa2a6d0cae3
sonjamehes <97450837+sonjamehes@users.noreply.github.com>
2023-07-19 14:03:55 +0200

    my version of the assignment for creating the constructors standing

---
+++ notebooks/f1-project/transformations/3.constructor_standings_mine.py
@@ -0,0 +1,28 @@
+# Databricks notebook source
+# MAGIC %run "../includes/config"
+
+# COMMAND ----------
+
+drivers_df = spark.read.parquet(f'{presentation_folder_path}/driver_standings')
+
+# COMMAND ----------
+
+display(drivers_df)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import sum, when, col, count
+
+# COMMAND ----------
+
+constructor_df = drivers_df \
+    .groupBy('race_year','team') \
+    .agg(sum('total_points').alias('total_points'), sum('wins').alias('wins'))
+
+# COMMAND ----------
+
+from pyspark.sql.functions import desc
+
+# COMMAND ----------
+
+display(constructor_df.filter('race_year = 2020').orderBy(desc('wins')))
