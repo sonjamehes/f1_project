edd63ade7f469ba9ee356b455a1f32dd9f1506c8
sonjamehes <97450837+sonjamehes@users.noreply.github.com>
2023-07-20 14:22:12 +0200

    visualisations of dominant drivers

---
+++ notebooks/f1-project/analysis/3.viz_dominant_drivers.sql
@@ -0,0 +1,54 @@
+-- Databricks notebook source
+CREATE OR REPLACE TEMP VIEW v_dominant_drivers
+AS
+select 
+    driver_name,
+    count(1) as total_races,
+    sum(calculated_points) as total_points,
+    avg(calculated_points) as avg_points,
+    RANK() over ( order by avg(calculated_points) desc) as driver_rank
+  from f1_presentation.calculated_race_results
+group by driver_name
+having count(1) > 50
+order by avg_points desc
+
+-- COMMAND ----------
+
+select 
+    race_year,
+    driver_name,
+    count(1) as total_races,
+    sum(calculated_points) as total_points,
+    avg(calculated_points) as avg_points
+  from f1_presentation.calculated_race_results
+where driver_name in (select driver_name from v_dominant_drivers where driver_rank <= 10)
+group by race_year, driver_name
+order by race_year, avg_points desc
+
+
+
+-- COMMAND ----------
+
+select 
+    race_year,
+    driver_name,
+    count(1) as total_races,
+    sum(calculated_points) as total_points,
+    avg(calculated_points) as avg_points
+  from f1_presentation.calculated_race_results
+where driver_name in (select driver_name from v_dominant_drivers where driver_rank <= 10)
+group by race_year, driver_name
+order by race_year, avg_points desc
+
+-- COMMAND ----------
+
+select 
+    race_year,
+    driver_name,
+    count(1) as total_races,
+    sum(calculated_points) as total_points,
+    avg(calculated_points) as avg_points
+  from f1_presentation.calculated_race_results
+where driver_name in (select driver_name from v_dominant_drivers where driver_rank <= 10)
+group by race_year, driver_name
+order by race_year, avg_points desc
