cfc54676621db7dd54fb41de1dea9100b0905445
sonjamehes <97450837+sonjamehes@users.noreply.github.com>
2023-07-20 13:41:29 +0200

    dominant teams assignment

---
+++ notebooks/f1-project/analysis/2.find_dominant_teams.sql
@@ -0,0 +1,25 @@
+-- Databricks notebook source
+select 
+    team_name,
+    count(1) as total_races,
+    sum(calculated_points) as total_points,
+    avg(calculated_points) as avg_points
+  from f1_presentation.calculated_race_results
+    where race_year between 2010 and 2020
+group by team_name
+having count(1) > 50
+order by avg_points desc
+
+
+-- COMMAND ----------
+
+select 
+    team_name,
+    count(1) as total_races,
+    sum(calculated_points) as total_points,
+    avg(calculated_points) as avg_points
+  from f1_presentation.calculated_race_results
+    where race_year between 2001 and 2010
+group by team_name
+having count(1) > 50
+order by avg_points desc
