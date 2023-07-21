ca2874c4fed5ac3456d0ab33b4395e50d4338177
sonjamehes <97450837+sonjamehes@users.noreply.github.com>
2023-07-21 14:52:03 +0200

    adding file_date column to the results table

--- notebooks/f1-project/transformations/race_results.py
+++ notebooks/f1-project/transformations/race_results.py
@@ -25,9 +25,10 @@ drivers = spark.read.parquet(f'{processed_folder_path}/drivers').withColumnRenam
 races = spark.read.parquet(f'{processed_folder_path}/races').withColumnRenamed("race_timestamp", "race_date").withColumnRenamed("name", "race_name")#.filter('race_year = 2020')
 circuits = spark.read.parquet(f'{processed_folder_path}/circuits').withColumnRenamed("location", "circuit_location")#.filter('circuit_location = "Abu Dhabi"')
 constructors = spark.read.parquet(f'{processed_folder_path}/constructors').withColumnRenamed("name", "team")
-results = spark.read.parquet(f'{processed_folder_path}/results').withColumnRenamed("time", "race_time") \
+results = spark.read.parquet(f'{processed_folder_path}/results').filter(f"file_date = '{v_file_date}'") \
+                                                                .withColumnRenamed("time", "race_time") \
                                                                 .withColumnRenamed("race_id", "result_race_id") \
-                                                                .filter(f"file_date = '{v_file_date}'")
+                                                                .withColumnRenamed("file_date", "result_file_date") 
 
 # COMMAND ----------
 
@@ -39,12 +40,17 @@ results_driver = results.join(drivers, results.driver_id == drivers.driver_id, '
                         .join(races, results.result_race_id == races.race_id, 'inner') \
                         .join(circuits, races.circuit_id == circuits.circuit_id, 'inner') \
                         .join(constructors, results.constructor_id == constructors.constructor_id, 'inner') \
-                        .select(races.race_id, races.race_year, races.race_name, races.race_date,circuits.circuit_location, drivers.driver_name, drivers.driver_number, drivers.driver_nationality,constructors.team, results.grid, results.fastest_lap, results.race_time, results.points, results.position ) 
+                        .select(races.race_id, races.race_year, races.race_name, races.race_date,circuits.circuit_location, drivers.driver_name, drivers.driver_number, drivers.driver_nationality,constructors.team, results.grid, results.fastest_lap, results.race_time, results.points, results.position, results.result_file_date) \
+                        .withColumnRenamed("result_file_date", "file_date")
 
 
 # COMMAND ----------
 
-results_driver = add_ingestion_date(results_driver)
+display(results_driver)
+
+# COMMAND ----------
+
+results_driver_final = add_ingestion_date(results_driver)
 
 # COMMAND ----------
 
@@ -52,18 +58,27 @@ results_driver = add_ingestion_date(results_driver)
 
 # COMMAND ----------
 
-overwrite_partition (results_driver, 'f1_presentation', 'race_results', 'race_id')
+overwrite_partition (results_driver_final, 'f1_presentation', 'race_results', 'race_id')
+
+# COMMAND ----------
+
+# %sql
+# select * from f1_presentation.race_results
+
+# COMMAND ----------
+
+# %sql
+# describe f1_presentation.race_results
+
+# COMMAND ----------
+
+# display(spark.read.parquet(f"{presentation_folder_path}/race_results"))
+
+# COMMAND ----------
+
+# %sql
+# drop table f1_presentation.race_results
 
 # COMMAND ----------
 
 dbutils.notebook.exit('Success')
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC refresh table f1_presentation.resultas
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select * from f1_presentation.race_results
