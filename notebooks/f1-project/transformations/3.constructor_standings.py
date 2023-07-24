76bb8e67e69040835ab346d91b0182df02e4dc3e
sonjamehes <97450837+sonjamehes@users.noreply.github.com>
2023-07-24 15:26:03 +0200

    update constructor standings

--- notebooks/f1-project/transformations/3.constructor_standings.py
+++ notebooks/f1-project/transformations/3.constructor_standings.py
@@ -1,5 +1,14 @@
 # Databricks notebook source
+dbutils.widgets.text('param_file_date', "2021-03-21")
+v_file_date = dbutils.widgets.get('param_file_date')
+
+# COMMAND ----------
+
 # MAGIC %run "../includes/config"
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/common_functions"
 
 # COMMAND ----------
 
@@ -15,14 +24,25 @@ from pyspark.sql.functions import sum, when, col, count
 
 # COMMAND ----------
 
+race_results_df_list = spark.read.parquet(f'{presentation_folder_path}/race_results')\
+    .filter(f"file_date='{v_file_date}'" )
+
+
+# COMMAND ----------
+
+race_year_list = dataframe_columns_to_list(race_results_df_list, 'race_year')
+
+# COMMAND ----------
+
+race_results_df = spark.read.parquet(f'{presentation_folder_path}/race_results')\
+    .filter(col("race_year").isin(race_year_list))
+
+# COMMAND ----------
+
 constructor_standings_df = race_results_df \
     .groupBy('race_year','team') \
     .agg(sum('points').alias('total_points'),
          count(when(col('position')== 1, True)).alias('wins'))
-
-# COMMAND ----------
-
-display(constructor_standings_df.filter('race_year = 2020'))
 
 # COMMAND ----------
 
@@ -36,17 +56,14 @@ final_df = constructor_standings_df.withColumn('rank', rank().over(constructors_
 
 # COMMAND ----------
 
-display(final_df.filter('race_year = 2020'))
+
+overwrite_partition(final_df, 'f1_presentation', 'constructor_standings', 'race_year')
 
 # COMMAND ----------
 
-final_df.write.mode('overwrite').parquet(f'{presentation_folder_path}/constructor_standings')
+dbutils.notebook.exit('Success')
 
 # COMMAND ----------
 
-# display(dbutils.fs.mounts())
-
-# COMMAND ----------
-
-# %fs
-# ls /mnt/f1datalakelearn/presentation-gold
+# %sql
+# drop table f1_presentation.driver_standings
