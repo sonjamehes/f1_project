c9c7164f47accd639fcd587e0fa866280e2c4ab6
sonjamehes <97450837+sonjamehes@users.noreply.github.com>
2023-07-25 10:27:07 +0200

    updating to handle incremental for partitioned tables (into delta)

--- notebooks/f1-project/ingestion/7.ingest_lap_times_files_folder.py
+++ notebooks/f1-project/ingestion/7.ingest_lap_times_files_folder.py
@@ -98,7 +98,12 @@ lap_times_renamed = add_ingestion_date(lap_times_renamed)
 
 # COMMAND ----------
 
-overwrite_partition (lap_times_renamed, 'f1_processed', 'lap_times', 'race_id')
+# overwrite_partition (lap_times_renamed, 'f1_processed', 'lap_times', 'race_id')
+
+# COMMAND ----------
+
+merge_condition = 'tgt.race_id = src.race_id  AND tgt.driver_id = src.driver_id  AND tgt.lap = src.lap'
+merge_delta_data(lap_times_renamed, 'f1_processed', 'lap_times', processed_folder_path, merge_condition, 'race_id')
 
 # COMMAND ----------
 
